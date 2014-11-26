c6875e4c89d51762109f493a740e99dc1f0e7ea6
qiao <529606091@qq.com>
2014-11-26 17:41:59 +0800

    MouseEvent

---
+++ MyDoSomething/MouseDownEvent/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MouseDownEvent
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        ObservableCollection<string> zoneList = new ObservableCollection<string>();
+      
+        private void PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Console.WriteLine((sender as FrameworkElement).Tag + " Preview");
+        }
+
+        private void MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Console.WriteLine((sender as FrameworkElement).Tag + " Bubble");
+        }
+
+        private void StackPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void ListView_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            //Console.WriteLine("P_Down");
+        }
+
+        private void ListView_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            //Console.WriteLine("Down");
+        }
+
+        private void ListView_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Console.WriteLine("Up");
+        }
+
+        private void ListView_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Console.WriteLine("P_LeftDown");
+        }
+
+        private void ListView_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Console.WriteLine("LeftDown");
+        }
+    }
+}
