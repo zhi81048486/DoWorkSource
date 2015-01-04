978e513e6db5bd79ce1b3560147d4f3ec3d03c3c
qiao <529606091@qq.com>
2015-01-04 10:44:38 +0800

    路由事件

--- MyDoSomething/MouseButtonEvent/MainWindow.xaml.cs
+++ MyDoSomething/MouseButtonEvent/MainWindow.xaml.cs
@@ -24,84 +24,43 @@ namespace MouseButtonEvent
         public MainWindow()
         {
             InitializeComponent();
-            this.Loaded += MainWindow_Loaded;
-            mygrid.AddHandler(Grid.MouseLeftButtonDownEvent, new MouseButtonEventHandler(this.Grid_MouseLeftButtonDown), true);
-            btnGo.AddHandler(Button.MouseLeftButtonUpEvent, new MouseButtonEventHandler(this.Button_MouseLeftButtonDown), true);
-            textBox1.AddHandler(TextBox.MouseLeftButtonDownEvent, new MouseButtonEventHandler(this.textBox1_MouseLeftButtonDown), true);
+            
+            Button2.AddHandler(Button.MouseUpEvent, new MouseButtonEventHandler(Button2_MouseUp), true);
 
-        }
-        private void Grid_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        }    
+        private void Button2_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Grid_PreviewMouseLeftButtonDown");
+            MessageBox.Show("Button2.Clicked");
         }
 
-        private void Button_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        private void Button2_MouseUp(object sender, MouseButtonEventArgs e)
         {
-            MessageBox.Show("Button_PreviewMouseLeftButtonDown");
+            MessageBox.Show("Button2.MouseUp");
         }
-        private void textBox1_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        private void AddButton2_MouseUp(object sender,RoutedEventArgs e)
         {
-            MessageBox.Show("textBox1_PreviewMouseLeftButtonDown");
-        }
-
-        private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            MessageBox.Show("Grid_MouseLeftButtonDown");
-        }
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Button_Click");
-        }
-        private void textBox1_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            MessageBox.Show("textBox1_MouseLeftButtonDown");
-        }
-        private void Button_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            MessageBox.Show("Button_MouseLeftButtonDown");
-        }
-        List<Person> p1;
-        void MainWindow_Loaded(object sender, RoutedEventArgs e)
-        {
-             p1= new List<Person>() { new Person() { Name = "Liy", Age = "22", Sex = "G" }, new Person() { Name = "Lucy", Age = "23", Sex = "B" } };
-            //MyListView.AddHandler(ListView.MouseLeftButtonDownEvent, new MouseButtonEventHandler(MyListView_MouseLeftButtonDown), true);
-             Changed(p1);
+            MessageBox.Show("Add Button2.MouseUP");
         }
 
-
-        //private void MyListView_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        //{
-        //    MessageBox.Show("PreviewMouseLeftButtonDown");
-        //}
-
-        //private void MyListView_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-        //{
-        //    MessageBox.Show("PreviewMouseLeftButtonUP");
-        //    e.Handled = false;
-        //}
-
-        //private void MyListView_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-        //{
-        //    MessageBox.Show("MouseLeftButtonUp");
-        //}
-        //private void MyListView_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        //{
-        //    MessageBox.Show("MouseLeftButtonDown");
-        //}
-
-        void Changed(List<Person> listC)
+        private void Button2_PreviewMouseDown(object sender, MouseButtonEventArgs e)
         {
-            List<Person> p2 = new List<Person>();
-            p2 = listC;
-            p2.Add(new Person() { Name = "Tom", Age = "12", Sex = "B" });
-            Debug.WriteLine("p2: "+p2.Count.ToString());
-            Debug.WriteLine("P1: "+p1.Count.ToString());
+            MessageBox.Show("Preview Event");
         }
-    }
-    public class Person
-    {
-        public string Name { get; set; }
-        public string Age { get; set; }
-        public string Sex { get; set; }
+
+        private void textBox1_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("TextBox_Double");
+            e.Handled = false;
+        }
+
+        private void btnGo_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Button_Double");
+        }
+
+        private void btnGo_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("ButtonClicked");
+        }
     }
 }
