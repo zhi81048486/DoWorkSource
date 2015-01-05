2c3bc4ae3aa050e7a104d9330f31f815aea62023
qiao <529606091@qq.com>
2015-01-05 17:40:38 +0800

    DateTime实现

--- MyDoSomething/DateTimeControl/MainWindow.xaml.cs
+++ MyDoSomething/DateTimeControl/MainWindow.xaml.cs
@@ -24,20 +24,88 @@ namespace DateTimeControl
         {
             InitializeComponent();
             this.Loaded += MainWindow_Loaded;
-            DateTime currentDate = DateTime.Now;
-            DateTime d = new DateTime(currentDate.Year, currentDate.Month, 1);
+            Button_MonthAdd.Click += Button_MonthAdd_Click;
+            Button_MonthReduce.Click += Button_MonthReduce_Click;
+            Combo_Monthes.SelectionChanged += Combo_Monthes_SelectionChanged;
+            Combo_Years.SelectionChanged += Combo_Years_SelectionChanged;
+        }
+        bool fist = true;
+        void Combo_Years_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (fist)
+            {
+                fist = false;
+                return;
+            }
+            DateTime dd = new DateTime((int)this.Combo_Years.SelectedValue, (int)this.Combo_Monthes.SelectedValue, 1);
+            InitMonthDay(dd);
+        }
+
+        void Combo_Monthes_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (fist)
+            {
+                fist = false;
+                return;
+            }
+            DateTime dd = new DateTime((int)this.Combo_Years.SelectedValue, (int)this.Combo_Monthes.SelectedValue, 1);
+            InitMonthDay(dd);
+        }
+
+        void Button_MonthReduce_Click(object sender, RoutedEventArgs e)
+        {
+            if (currentDate.Month == 1)
+            {
+                Button_MonthReduce.IsEnabled = false;
+                return;
+            }
+            DateTime dd = new DateTime(currentDate.Year, currentDate.Month - 1, 1);
+            InitMonthDay(dd);
+            if (currentDate.Month != 12)
+                Button_MonthAdd.IsEnabled = true;
+        }
+
+        void Button_MonthAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if (currentDate.Month == 12)
+            {
+                Button_MonthAdd.IsEnabled = false;
+                return;
+            }
+            DateTime dd = new DateTime(currentDate.Year, currentDate.Month + 1, 1);
+
+            InitMonthDay(dd);
+            if (currentDate.Month != 1)
+                Button_MonthReduce.IsEnabled = true;
         }
         private Label[,] _btnMonthMode = new Label[6, 7];
 
         void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
             DateLoading();
+
+            List<int> years = new List<int>();
+           
+            for (int i = 0; i < 10; i++)
+            {
+                years.Add(2010+i);
+            }
+            Combo_Years.SelectedIndex = 5;
+            Combo_Years.ItemsSource = years;
+            List<int> monthes = new List<int>();
+            for (int i = 0; i < 12; i++)
+            {
+                monthes.Add(i+1);
+            }
+            Combo_Monthes.SelectedIndex = DateTime.Now.Month - 1;
+            Combo_Monthes.ItemsSource = monthes;
+
         }
 
         void DateLoading()
         {
-         //Label_Date.Content = string.Format("{0:yyyy年MM月dd}", DateTime.Now);
-
+            currentDate = DateTime.Now;
+            Label_Date.Content = string.Format("{0:yyyy年MM月dd日}", currentDate);
             for (int i = 1; i <= 7; i++)
             {
                 Label lbl = new Label();
@@ -52,25 +120,77 @@ namespace DateTimeControl
                 for (int j = 0; j < 7; j++)
                 {
                     Label l1 = new Label();
+                    l1.Background = Brushes.Pink;
+                   // l1.Margin = new Thickness(4);
+                    l1.BorderBrush = Brushes.Black;
+                    l1.BorderThickness = new Thickness(1);
+                  
+                    l1.MouseLeftButtonDown += l1_MouseLeftButtonDown;
                     DayGrid.Children.Add(l1);
                     _btnMonthMode[i, j] = l1;
-
                 }
             }
             InitMonthDay(DateTime.Now);
         }
+
+        void l1_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+          //  Console.WriteLine(((e.Source) as Label).Content);
+            currentDate = new DateTime(currentDate.Year, currentDate.Month, (int)((e.Source) as Label).Content);
+            Label_Date.Content = string.Format("{0:yyyy年MM月dd日}", currentDate);
+
+        }
+
+        DateTime currentDate;
         void InitMonthDay(DateTime date)
         {
-            DateTime currentDate = date;
-            DateTime d = new DateTime(currentDate.Year, currentDate.Month+3, 1);
-            int days = DateTime.DaysInMonth(currentDate.Year, currentDate.Month+1);
-            int fstCol = (int)d.DayOfWeek;
+            currentDate = date;
+            Label_Date.Content = string.Format("{0:yyyy年MM月dd日}", currentDate);
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    _btnMonthMode[i,j].Content = "";
+                }
+            }
+            DateTime d = new DateTime(date.Year, date.Month, 1);
+            int days = DateTime.DaysInMonth(date.Year, date.Month);
+            int fstCol = 0;
+            //设置周几就是第几天(因为默认周日是第一天，所以改成周一是第一天)
+            switch (d.DayOfWeek)
+            {
+                case DayOfWeek.Friday:
+                    fstCol = 4;
+                    break;
+                case DayOfWeek.Monday:
+                    fstCol = 0;
+                    break;
+                case DayOfWeek.Saturday:
+                    fstCol = 5;
+                    break;
+                case DayOfWeek.Sunday:
+                    fstCol = 6;
+                    break;
+                case DayOfWeek.Thursday:
+                    fstCol = 3;
+                    break;
+                case DayOfWeek.Tuesday:
+                    fstCol = 1;
+                    break;
+                case DayOfWeek.Wednesday:
+                    fstCol = 2;
+                    break;
+                default:
+                    break;
+            }
+            //k为一个月的天数的计数
             int k = 1;
             for (int i = 0; i < 6; i++)
             {
                 for (int j = 0; j < 7; j++)
                 {
-                    if (i == 0 && j < fstCol-1)
+                    //从每个月第一天的星期几开始往后计数
+                    if (i == 0 && j < fstCol)
                         continue;
                     else
                     {
@@ -81,7 +201,7 @@ namespace DateTimeControl
                     }
 
                 }
-            }           
+            }
         }
 
 
@@ -91,7 +211,6 @@ namespace DateTimeControl
             {
                 case 1:
                     return "一";
-
                 case 2:
                     return "二";
 
