d5b2c26ab42ccd553b8eb1389604167bf0a97aba
qiao <529606091@qq.com>
2015-03-04 18:20:26 +0800

    52

---
+++ MyDoSomething/MyControlTemplate/Window1.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MyControlTemplate
+{
+    /// <summary>
+    /// Window1.xaml 的交互逻辑
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+            MylistView.ItemsSource = getsou();
+        }
+
+        List<sou> getsou()
+        {
+            List<sou> l = new List<sou>();
+            l.Add(new sou() { ff = "ffff", dd = "dddd" });
+            l.Add(new sou() { ff = "ffff", dd = "dddd" }); l.Add(new sou() { ff = "ffff", dd = "dddd" }); l.Add(new sou() { ff = "ffff", dd = "dddd" }); l.Add(new sou() { ff = "ffff", dd = "dddd" });
+            return l;
+        }
+
+        public class sou
+        {
+            public string ff { get; set; }
+            public string dd { get; set; }
+        }
+    }
+}
