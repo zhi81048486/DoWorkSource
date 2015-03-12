b0e8cc7b2ee04d251899c3bcd2765252f64cad4e
qiao <529606091@qq.com>
2015-03-12 18:18:01 +0800

    2

--- MyDoSomething/MyConverter/BoolToVisible.cs
+++ MyDoSomething/MyConverter/BoolToVisible.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows;
 using System.Windows.Data;
 
 namespace MyConverter
@@ -31,6 +32,27 @@ namespace MyConverter
                 else
                     return false;
             }
+            return false;
+        }
+    }
+
+    public class BoolToMargin : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (null != value && value.GetType() == typeof(bool))
+            {
+                bool b = (bool)value;
+                if (b)
+                    return new Thickness(0,0,30,0);
+                else
+                    return new Thickness(-3000, 0, 0, 0);
+            }
+            return System.Windows.Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
             return false;
         }
     }
