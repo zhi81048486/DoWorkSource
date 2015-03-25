cbecf4b16c8b143c10eaae696dad5e81aeeeadaf
Joe_Home <qiaohengbo@outlook.com>
2015-03-25 20:27:40 +0800

    冲突解决

---
+++ MyDoSomething/MyControlTemplate/B_T_Panel.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MyControlTemplate
+{
+    /// <summary>
+    /// B_T_Panel.xaml 的交互逻辑
+    /// </summary>
+    public partial class B_T_Panel : Window
+    {
+        public B_T_Panel()
+        {
+            InitializeComponent();
+        }
+    }
+}
