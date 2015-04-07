4b9c0038f0d3efe30c8287c9f7e4732d7df8ff6e
qiao <529606091@qq.com>
2015-04-07 18:03:08 +0800

    tree

---
+++ MyDoSomething/ImageOperation/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ImageOperation
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void UIElement_OnMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            var st = (ScaleTransform)image.RenderTransform;
+            double zoom = e.Delta > 0 ? .2 : -.2;
+            st.ScaleX += zoom;
+            st.ScaleY += zoom;
+        }
+        Point start;
+        Point origin;
+        private void Image_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            image.CaptureMouse();
+            var tt = (TranslateTransform)((TransformGroup)image.RenderTransform)
+                .Children.First(tr => tr is TranslateTransform);
+            start = e.GetPosition(border);
+            origin = new Point(tt.X, tt.Y);
+        }
+
+        private void Image_OnMouseMove(object sender, MouseEventArgs e)
+        {
+            if (image.IsMouseCaptured)
+            {
+                var tt = (TranslateTransform)((TransformGroup)image.RenderTransform)
+                    .Children.First(tr => tr is TranslateTransform);
+                Vector v = start - e.GetPosition(border);
+                tt.X = origin.X - v.X;
+                tt.Y = origin.Y - v.Y;
+            }
+        }
+
+        private void Image_OnMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            image.ReleaseMouseCapture();
+        }
+    }
+}
