c0b8de16c9c481476228ed967573307d66e94d55
qiao <529606091@qq.com>
2015-04-10 18:07:28 +0800

    彩旗飘飘

---
+++ MyDoSomething/AnimatedFlag/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AnimatedFlag
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int ctr = 1;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DoubleAnimation_Completed(object sender, EventArgs e)
+        {
+            ShowImage();
+            da.BeginAnimation(Image.WidthProperty, da);
+        }
+
+        private void ShowImage()
+        {
+            string filename = "Images/Flag" + ctr + ".jpg";
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            image.UriSource = new Uri(filename, UriKind.Relative);
+            image.EndInit();
+            flagImage.Source = image;
+            ctr++;
+            if (ctr > 6)
+            {
+                ctr = 1;
+            }
+        }
+    }
+}
