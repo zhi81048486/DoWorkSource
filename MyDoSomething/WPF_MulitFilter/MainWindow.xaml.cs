633ff63fa57d94faf3ec6e6f7e73b0d19524e88a
Joe_Home <qiaohengbo@outlook.com>
2015-05-05 23:51:52 +0800

    多条件筛选

--- MyDoSomething/WPF_MulitFilter/MainWindow.xaml.cs
+++ MyDoSomething/WPF_MulitFilter/MainWindow.xaml.cs
@@ -34,29 +34,103 @@ namespace WPF_MulitFilter
             Console.WriteLine(this.ListView_Filter.Items.Count);
         }
 
-        private CollectionViewSource CVS;
+        private CollectionView CVS;
         private ObservableCollection<Persons> lists;
+        private List<Persons> DonePersons = new List<Persons>();
         void DoSource()
         {
             lists = new ObservableCollection<Persons>()
             {
-                new Persons(){Name = "Llly",Age="53",Country = "USA",Sex="Girl"},
+                new Persons(){Name = "Llly",Age="23",Country = "USA",Sex="Girl"},
                 new Persons(){Name = "Tom",Age="23",Country = "USA",Sex="Girl"},
                 new Persons(){Name = "Josn",Age="13",Country = "Mexico",Sex="Girl"},
                 new Persons(){Name = "James",Age="63",Country = "Canada",Sex="Boy"},
-                new Persons(){Name = "Joe",Age="20",Country = "Canada",Sex="Boy"},
+                new Persons(){Name = "Joe",Age="23",Country = "Canada",Sex="Boy"},
                 new Persons(){Name = "Han",Age="18",Country = "Japan",Sex="Boy"},
                 new Persons(){Name = "Lin",Age="15",Country = "England",Sex="Girl"},
                 new Persons(){Name = "Den",Age="26",Country = "China",Sex="Girl"},
                 new Persons(){Name = "Sun",Age="24",Country = "China",Sex="Boy"},
             };
-            CVS = new CollectionViewSource() { Source = lists };
+            CVS = (CollectionView)CollectionViewSource.GetDefaultView(lists);
             //ListView_Filter.ItemsSource = CVS;
-            lists.Contains(new Persons());
-
+            
             Binding binding = new Binding();
             binding.Source = CVS;
             BindingOperations.SetBinding(ListView_Filter, ListView.ItemsSourceProperty, binding);
+
+            ComboBox_Name.ItemsSource = from r in lists select r.Name;
+            ComboBox_Age.ItemsSource = from r in lists select r.Age;
+            ComboBox_Country.ItemsSource = from r in lists select r.Country;
+            ComboBox_Sex.ItemsSource = from r in lists select r.Sex;
+            ComboBox_Age.SelectionChanged += ComboBox_Age_SelectionChanged;
+            ComboBox_Name.SelectionChanged += ComboBox_Name_SelectionChanged;
+            ComboBox_Country.SelectionChanged += ComboBox_Country_SelectionChanged;
+            isLoad = true;
+        }
+
+        private bool isLoad = false;
+        void ComboBox_Country_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            IEnumerable<Persons> Coun;
+            var _country = ComboBox_Country.SelectedItem;
+            if (!isLoad)
+            { Coun = from i in DonePersons where i.Country == _country select i; }
+            else
+            {
+                Coun = from i in lists where i.Country == _country select i;
+            }
+            CVS.Filter += (p) =>
+            {
+                foreach (var c in Coun)
+                {
+                    if (((Persons)p).Country.Equals(c.Country))
+                        return true;
+                    else
+                    {
+                        return false;
+                    }
+                }
+                return false;
+            };
+            DonePersons = Coun.ToList();
+            isLoad = false;
+        }
+
+        void ComboBox_Name_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var r = ComboBox_Name.SelectedItem;
+
+        }
+
+        void ComboBox_Age_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            IEnumerable<Persons> items;
+            var _age = ComboBox_Age.SelectedItem;
+            if (!isLoad)
+            {
+                items = from i in DonePersons where i.Age == _age select i;
+            }
+            else
+            {
+                items= from i in lists where i.Age == _age select i;
+                
+            }
+            CVS.Filter += (p) =>
+            {
+                foreach (var i in items)
+                {
+                    if (((Persons)p).Age.Equals(i.Age))
+                        return true;
+                    else
+                    {
+                        return false;
+                    }
+                }
+                return false;
+
+            };
+            DonePersons = items.ToList();
+            isLoad = false;
         }
 
         void AddPerson()
