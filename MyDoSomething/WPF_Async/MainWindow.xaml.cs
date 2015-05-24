a5197f25ff795e86c3bf6e3cf1c018ef78327ac3
Joe_Home <qiaohengbo@outlook.com>
2015-05-24 21:42:01 +0800

    some error

---
+++ MyDoSomething/WPF_Async/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Async
+{
+    public delegate void AsyncDel();
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public delegate void AsyncDel2();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            AsyncDel _asyncDel = TakeLongWhile;
+            AsyncDel2 _asyncDel2 = TakeLongWhile;
+        }
+        #region 委托异步
+        //委托的异步调用使用了线程池中的线程
+
+        //这里必须是静态的方法，因为csharp规定只能在类的内部定义变量或者属性，并初始化。不能变量直接引用变量。
+        //like:int a=10; int temp=a;
+      //AsyncDel  _asyncDel= TakeLongWhile ;
+
+        #endregion
+
+
+        public void TakeLongWhile()
+        {
+            ShowTextBox("方法开始调用：");
+            Thread.Sleep(5000);
+            ShowTextBox("方法调用结束。");
+
+        }
+
+        public void ShowTextBox(string str)
+        {
+            TextBox_Info.Text = DateTime.Now.ToShortTimeString() + str + "\n";
+        }
+
+
+    }
+
+}
---
+++ MyDoSomething/WPF_Error/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Error
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region 字段初始值设定无法引用非静态的字段，方法，属性
+        //#region Code
+        ///*C#规定在类内部只能定义属性或者变量，并初始化，不能直接变量引用变量。在初始化类实例之前就调用了字段.c# 中字段的初始化先于构造函数。*/
+        //private int a = 1;
+        //private int tem = a;
+
+        //private delegate void someDel();
+
+        //private someDel _someDel = method;
+
+        //public void method()
+        //{
+
+        //}
+
+        //public void Here()
+        //{
+        //    int a = 1;
+        //    int temp = a;
+
+        //    someDel _someDel = method;
+        //} 
+        //#endregion
+        #endregion
+    }
+}
