323843285cbbf4379f7741aaacacf4b2bf9c494d
Qiao_Work <529606091@qq.com>
2015-05-25 18:20:23 +0800

    new

---
+++ MyDoSomething/WPFStory/WindowShowStory.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFStory
+{
+    /// <summary>
+    /// WindowShowStory.xaml 的交互逻辑
+    /// </summary>
+    public partial class WindowShowStory : Window
+    {
+        public WindowShowStory()
+        {
+            InitializeComponent();
+        }
+    }
+}
